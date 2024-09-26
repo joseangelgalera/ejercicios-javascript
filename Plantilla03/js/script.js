c78ec47c41d70c888d9a0294bed66dc328b38e18
joseangelgalera <joseangelgg96@hotmail.com>
2024-09-26 12:14:10 +0200

    Cambiado tipo de login

--- Plantilla03/js/script.js
+++ Plantilla03/js/script.js
@@ -1,27 +1,61 @@
-"use strict"
+"use strict";
 
-window.onload = function() {
-    login();
-};
-
-function login() {
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.querySelector('.login-form form');
+    var login = document.querySelector('.login-form');
     var container = document.querySelector('.container');
 
-    var nombre = prompt("Por favor, ingresa tu nombre:");
+    container.style.display = 'none';
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+
+        if (username === "Jose" && password === "1234") {
+            alert("¡Bienvenido, " + username + "!");
+            login.style.display = 'none';
+            container.style.display = 'flex';
+        } else {
+            alert("Nombre de usuario o contraseña incorrectos");
+            document.getElementById('username').value = '';
+            document.getElementById('password').value = '';
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+// "use strict"
+
+
+// function login() {
+//     var container = document.querySelector('.container');
+
+//     var nombre = prompt("Por favor, ingresa tu nombre de usuario:");
     
-    while (nombre !== "Jose") {
-        alert("Nombre no ingresado. Por favor, intenta nuevamente.");
-        nombre = prompt("Por favor, ingresa tu nombre:");
-    }
+//     while (nombre !== "Jose") {
+//         alert("Nombre de usuario o contraseña incorrectos");
+//         nombre = prompt("Por favor, ingresa tu nombre:");
+//     }
     
-    var contraseña = prompt("Hola " + nombre + "! Por favor, ingresa tu contraseña:");
+//     var contraseña = prompt("Hola " + nombre + "! Por favor, ingresa tu contraseña:");
         
-    while (contraseña !== "1234") {
-        alert("Contraseña incorrecta. Por favor, intenta nuevamente.");
-        nombre = prompt("Por favor, ingresa tu nombre:");
-        contraseña = prompt("Hola " + nombre + "! Por favor, ingresa tu contraseña:");
-    }
+//     while (contraseña !== "1234") {
+//         alert("¿Desea intentarlo de nuevo?");
+//         nombre = prompt("Por favor, ingresa tu nombre de usuario:");
+//         contraseña = prompt("Hola " + nombre + "! ingrese su contraseña:");
+//     }
     
-    alert("¡Bienvenido, " + nombre + "! Has iniciado sesión exitosamente.");
-    container.style.display = 'flex';
-}
+//     alert("¡Bienvenido, " + nombre + "!");
+//     container.style.display = 'flex';
+// }
