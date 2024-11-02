8b59d5933c154deba55dbbc608ad985be9071a14
joseangelgalera <joseangelgg96@gmail.com>
2024-11-02 16:18:08 +0100

    Pequeñas modificaciones

--- Plantilla03/numbers/calculadora/js/script.js
+++ Plantilla03/numbers/calculadora/js/script.js
@@ -11,6 +11,12 @@ const borrar = document.getElementById("borrar");
 const limpiarC = document.getElementById("limpiarC");
 const limpiarT = document.getElementById("limpiarT");
 const memoria = document.getElementById("memoria");
+const cerrarSesion = document.querySelector('.cerrar-sesion');
+const container = document.querySelector('.container');
+const login = document.querySelector('.login-form');
+const topnav = document.querySelector('.topnav');
+const welcome = document.querySelector('.welcome');
+const header = document.querySelector('.header');
 
 let x = "";
 let y = "";
@@ -91,4 +97,51 @@ memoria.addEventListener("click", function () {
         salida.innerHTML = memoriaValor; 
         entradaActual = memoriaValor; 
     }
+});
+
+var db;
+var request = indexedDB.open("loginDB", 1);
+
+request.onupgradeneeded = function(event) {
+    db = event.target.result;
+    var objectStore = db.createObjectStore("loginStatus", { keyPath: "key" });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+};
+
+request.onerror = function(event) {
+    console.error("Database error: " + event.target.errorCode);
+};
+
+
+cerrarSesion.addEventListener('click', function() {
+    window.location.href = "/Plantilla03/index.html";
+    sessionStorage.removeItem("username");
+    sessionStorage.removeItem("LoggedIn");
+
+    var transaction = db.transaction(["loginStatus"], "readwrite");
+    var objectStore = transaction.objectStore("loginStatus");
+    var request = objectStore.put({ key: "LoggedIn", value: false });
+
+    request.onsuccess = function(event) {
+        console.log("Login status updated to false.");
+        
+        container.style.display = 'none';
+        login.style.display = 'flex';
+        login.style.flexDirection = 'column';
+        topnav.style.display = 'none';
+
+        document.getElementById('username').value = '';
+        document.getElementById('password').value = '';
+
+        welcome.style.display = 'none';
+        header.style.borderRadius = ''; 
+
+    };
+
+    request.onerror = function(event) {
+        console.error("Unable to update login status: " + event.target.errorCode);
+    };
 });
